94878528e5961bedbf198a61f0160a854b106c4b
Lena Kujath <l.kujath@gmx.de>
2020-02-04 15:01:02 +0100

    changed Contact into a class

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,27 +1,45 @@
 import React from 'react';
-import './Contact.css'
+import './Contact.css';
 
-function Contact(props) {
 
+
+
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: true,
+      change : () => {                      
+        const newStatus = !this.state.online
+        this.setState({online : newStatus})
+          }   
+    } 
+  }
+
+  
+
+  render() {
     return (
-    
       <figure className="Contact">
-     <img className="avatar" src={props.avatar}></img>
-      <figcaption>
-      <p className="name"> {props.name}
-      </p>
-     <div className="status"> 
-     {props.isLoggedIn ? <div><div className="status-online"></div> <span className="status-text">Online</span>
-     </div>
-     :
-     <div> <div className="status-offline"></div>
-     <span className="status-text">Offline</span>
-     </div>
-     }
-     </div> 
-       </figcaption>
-        </figure>     
+        <img className="avatar" src={this.props.avatar}  />
+        <figcaption>
+          <p className="name" > {this.props.name}</p>
+          <div className="status"   
+                onClick={this.state.change}> 
+           {this.state.online ? <div><div className="status-online"></div> <span className="status-text">Online</span>
+            </div>
+              :
+            <div> <div className="status-offline"></div>
+            <span className="status-text">Offline</span>
+            </div>
+                      }
+                  </div>
+          
+        </figcaption>
+      </figure>
     );
   }
+}
+
 
 export default Contact;
